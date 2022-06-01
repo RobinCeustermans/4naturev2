03b93338ac472b77e1219a380e678d5596c46a82
Robin Ceustermans <roobiinboy@gmail.com>
2022-06-01 16:16:32 +0200

    home en bedrijven responsief en navbar beter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 import {FormGroup, FormControl} from '@angular/forms';
+import {hasClassName} from '@ng-bootstrap/ng-bootstrap/util/util';
+import { Router, NavigationEnd } from '@angular/router';
 
+//to use jquery
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
+
+
 export class AppComponent {
   title = '4nature';
 
+  constructor(private router: Router) {
 
-  constructor() {
   }
+
+
 
   ngOnInit(){
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) { return; }
 
+      // This closes the top nav
+      $(document).click((event: any) => {
+        const click = $(event.target);
+        const _open = $('.navbar-collapse').hasClass('show');
+        if (_open === true && !click.hasClass('navbar-toggler')) {
+          $('.navbar-toggler').click();
+        }
+      });
+    });
   }
+
+
 }
